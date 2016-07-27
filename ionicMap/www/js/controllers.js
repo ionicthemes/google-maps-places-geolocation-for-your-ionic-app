9fce3d7b80b3c6f0b166bf807f0fed3423ef7957
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-07-27 20:12:11 -0300

    Cambios en las dependencias, y se realiza la funcionalidad de search

--- ionicMap/www/js/controllers.js
+++ ionicMap/www/js/controllers.js
@@ -1,17 +1,33 @@
 angular.module('controllers', [])
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, NgMap) {
+  // var options = {timeout: 10000, enableHighAccuracy: true};
+  //
+  // $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+  //   var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //   var mapOptions = {
+  //     center: latLng,
+  //     zoom: 15,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //   // $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  // }, function(error){
+  //   console.log("Could not get location");
+  // });
 
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }, function(error){
-    console.log("Could not get location");
-  });
+  var vmap = this
+  NgMap.getMap().then(function(map){
+    var options = {timeout: 10000, enableHighAccuracy: true};
+    vmap.map = map;
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+      $scope.latitude = position.coords.latitude;
+      $scope.longitude = position.coords.longitude;
+    })
+
+  $scope.placeChanged = function() {
+    vmap.place = this.getPlace();
+    console.log('location', vmap.place.geometry.location);
+    vmap.map.setCenter(vmap.place.geometry.location);
+  }
+  })
 });
