6955949ce73cbcbe70ae68bf5646ebe1cc62dcf2
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-07-26 23:08:09 -0300

    Se muestra el mapa con la ubicacion actual

---
+++ ionicMap/www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('controllers', [])
+
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  }, function(error){
+    console.log("Could not get location");
+  });
+});
