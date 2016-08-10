d945fdd16297fac610759ce6ddd38eb949ec27c4
Agustin Haller <agustinhaller@gmail.com>
2016-08-10 16:53:10 -0300

    Add button to set user current location, clean map controls (just display the basic ones)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,20 @@
 angular.module('controllers', [])
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, NgMap) {
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, NgMap, $ionicLoading){
+  // Central Park location
+  $scope.latitude = 40.785091;
+  $scope.longitude = -73.968285;
 
   var vmap = this;
   NgMap.getMap().then(function(map){
-    var options = {timeout: 10000, enableHighAccuracy: true};
+    // var options = {timeout: 10000, enableHighAccuracy: true};
     vmap.map = map;
-    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-      $scope.latitude = position.coords.latitude;
-      $scope.longitude = position.coords.longitude;
-    })
+    // $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+    //   $scope.latitude = position.coords.latitude;
+    //   $scope.longitude = position.coords.longitude;
+    // })
+
+
   })
 
   $scope.customMarkers = [
@@ -22,6 +27,22 @@ angular.module('controllers', [])
     $scope.searchMarker={"latitude": vmap.place.geometry.location.lat(),
                         "longitude": vmap.place.geometry.location.lng()}
   }
+
+  $scope.centerOnCurrentPosition = function(){
+    var options = {
+      timeout: 10000,
+      enableHighAccuracy: true
+    };
+    $ionicLoading.show({
+      template: 'Getting current position ...'
+    });
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+      $ionicLoading.hide().then(function(){
+        $scope.latitude = position.coords.latitude;
+        $scope.longitude = position.coords.longitude;
+      });
+    });
+  };
 
 
 });
